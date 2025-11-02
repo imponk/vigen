47ec605cb2b91f3855aa65d12b75a2f4c4af49da
imponk <113895746+imponk@users.noreply.github.com>
2025-11-02 18:14:49 +0700

    Update videogen_beta.py

--- videogen_beta.py
+++ videogen_beta.py
@@ -1,7 +1,7 @@
 # ==========================================================
 # ✅ VIDEO GENERATOR BETA (FINAL STABLE WITH HIGHLIGHT)
 # ==========================================================
-from moviepy import ImageClip, CompositeVideoClip, concatenate_videoclips
+from moviepy.editor import ImageClip, CompositeVideoClip, concatenate_videoclips
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import os, re, sys, traceback, time
